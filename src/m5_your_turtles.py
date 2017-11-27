ec494ed373d01f86b9a79ce6b8e2de91558e896c
mazanyap <Mazanayap@rose-hulman.edu>
2017-11-27 15:47:20 -0500

    finished m5 its pretty cool

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Alex Mazany.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,36 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+circular_turtle = rg.SimpleTurtle('turtle')
+square_turtle = rg.SimpleTurtle('turtle')
+
+circular_turtle.pen = rg.Pen('red', 5)
+circular_turtle.speed = 20
+
+square_turtle.pen = rg.Pen('blue', 7)
+square_turtle.speed = 20
+
+size = 200
+
+for k in range(7):
+    circular_turtle.draw_circle(size)
+    circular_turtle.pen_up()
+    circular_turtle.left(20)
+    circular_turtle.backward(10)
+    circular_turtle.right(20)
+    circular_turtle.pen_down()
+
+    square_turtle.draw_square(size)
+    square_turtle.pen_up()
+    square_turtle.right(10)
+    square_turtle.backward(10)
+    square_turtle.left(30)
+    square_turtle.pen_down()
+
+    size = size - 20
+
+
