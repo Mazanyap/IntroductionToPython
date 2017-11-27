a029fed4a5cd8be295a2a3aa36d290e1fa4e1536
mazanyap <Mazanayap@rose-hulman.edu>
2017-11-27 09:37:38 -0500

    finished m2

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# Done: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# Done: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -60,6 +60,7 @@ dave = rg.SimpleTurtle()
 # ----------------------------------------------------------------------
 # Ask the SimpleTurtle objects to do things:
 # ----------------------------------------------------------------------
+dave.pen = rg.Pen('blue', 10)
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
@@ -68,7 +69,7 @@ dave.forward(200)
 # Construct a new turtle and ask it to do things.
 # ----------------------------------------------------------------------
 matt = rg.SimpleTurtle('turtle')
-matt.pen = rg.Pen('red', 30)
+matt.pen = rg.Pen('green', 100)
 matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
@@ -77,7 +78,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
+# Done: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +92,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# Done: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +110,13 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+shaun = rg.SimpleTurtle()
+shaun.forward(280)
+shaun.right(50)
+shaun.forward(50)
 ########################################################################
 #
-# TODO: 5.
+# Done: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
